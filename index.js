1591e386b6fa1bebf990ee1046952b5976be760b
Lovee Jain <lovee.jain@prezzee.com>
2025-05-01 16:50:12 +1000

    use genai package for gemini and vertexai implementations

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenAI } from "@google/genai";
 import express from "express";
 import dotenv from "dotenv";
 const app = express();
@@ -7,34 +7,49 @@ app.use(express.json());
 dotenv.config();
 const port = process.env.PORT || 3000;
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-let model;
-let chat;
+const geminiAI = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
+const vertexAI = new GoogleGenAI({
+  vertexai: true,
+  project: "your-project-id",
+  location: "us-central1",
+})
 
 app.listen(port, async () => {
-  console.log(`Server is running on port ${port}`);
-  model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
-    systemInstruction: "You are an expert news reporter who curates content and provides a brief to the point response in Australian slang. You do not give long paragraphs but just some bullet points with the summary.",
-    tools: [{
-      googleSearchRetrieval: {
-        dynamicRetrievalConfig: {
-          mode: "MODE_DYNAMIC",
-          dynamicThreshold: 0.3,
-        }
-      }
-    }],
-  });
-  
+  console.log(`Server is running on port ${port}`);  
 });
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/news", async (req, res) => {
-  chat = await model.startChat();
+app.get("/news-gemini", async (req, res) => {
   const prompt = "latest tech news in last 24 hours"
-  const result = await chat.sendMessage(prompt);
-  res.send(result.response.text());
+  const chat = await geminiAI.chats.create({
+    model: "gemini-2.5-flash-preview-04-17",
+    config: {
+      tools: [
+        {
+          googleSearch: {}
+        }
+      ]
+    }
+  })
+  const result = await chat.sendMessage({"message": "who is the president of the USA?"}) // use prompt as the message, this is for testing
+  res.send(result.text);
+})
+
+app.get("/news-vertex", async (req, res) => {
+  const prompt = "latest tech news in last 24 hours"
+  const chat = await vertexAI.chats.create({
+    model: "gemini-2.5-flash-preview-04-17",
+    config: {
+      tools: [
+        {
+          googleSearch: {}
+        }
+      ]
+    }
+  })
+  const result = await chat.sendMessage({"message": "who is the president of the USA?"}) // use prompt as the message, this is for testing
+  res.send(result.text);
 })
